556dc6ff60a15eb1ddaf3b315d43911746b6934e
Milind Dev <56596354+Milind-dev@users.noreply.github.com>
2025-06-07 21:49:21 +0530

    route sample

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ app.listen(3000, () => {
 
 app.get('/api/user/login',(req,res) =>{
   console.log(req)
-  const data = {name:'techyks',email:'milinddev101@gmail.com'}
-  res.status(200).send(data);
+  // const data = {name:'techyks',email:'milinddev101@gmail.com'}
+  res.json({
+    name:'miind',
+    email:'milind@gmail.com'
+  })
+  res.status(200).send(res);
 })
