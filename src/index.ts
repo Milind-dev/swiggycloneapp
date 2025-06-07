bc752f6b6acb0bae5031eebb9bba4ec7409680fc
Milind Dev <56596354+Milind-dev@users.noreply.github.com>
2025-06-07 21:47:07 +0530

    route sample

--- src/index.ts
+++ src/index.ts
@@ -6,72 +6,9 @@ app.listen(3000, () => {
   console.log("server start at 3000 ");
 });
 
-function is_MathByX_Available() {
-  return false;
-}
-function is_MathByY_Available() {
-  return false;
-}
-function is_SciByX_Available() {
-  return false;
-}
-function is_SciByY_Available() {
-  return false;
-}
 
-function resultMathsBooks() {
-  return new Promise((resolve, reject) => {
-    if (is_MathByX_Available()) {
-      resolve(true);
-    } else if (is_MathByY_Available()) {
-      resolve(true);
-    } else {
-      reject(false);
-    }
-  });
-}
-function resultScienceBook(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    if (is_SciByX_Available()) {
-      resolve("is_SciByX_Available");
-    } else if (is_SciByY_Available()) {
-      resolve("is_SciByY_Available");
-    } else {
-      reject("Books are not available");
-    }
-  });
-}
-
-resultMathsBooks().then((result) => {
-    console.log("result");
-    if(result){
-     resultScienceBook().then(data => {
-        console.log(data);
-     }).catch(e=>{
-        console.log(e);
-     })
-    }
-}).catch((error) => {
-    console.log(error);
-})
-
-
-async function final(){
-    try {
-        let result1 = await resultMathsBooks();
-        if(result1){
-            result1 = await resultScienceBook();
-            return result1;
-        }
-    } catch (error) {
-        // console.log(error);
-        return Promise.reject(error);
-    }
-}
-
-final().then((data) => {
-    console.log("data", data);
-}).catch(e =>{
-    console.log(e);
-    
+app.get('/api/user/login',(req,res) =>{
+  console.log(req)
+  const data = {name:'techyks',email:'milinddev101@gmail.com'}
+  res.status(200).send(data);
 })
